5c78c8a66c5f49c70afbdd45e650de6b71da9d08
hainguyen <haigom00@gmail.com>
2021-10-03 16:05:24 +0700

    build outpu

--- Server/src/routes/users.js
+++ Server/src/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a Root Dir');
+  res.send('respond with a Root Dir2');
 });
 
 module.exports = router;
